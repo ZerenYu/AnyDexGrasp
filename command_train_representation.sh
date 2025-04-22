09f9cb1a0253f059b4fd68bb0101f9a6f39bb2a6
ZerenYu <yuzeren@umich.edu>
2025-04-22 09:58:22 +0000

    try to run training

--- command_train_representation.sh
+++ command_train_representation.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python train.py --log_dir logs/model/rep_log --batch_size 4 --learning_rate 0.001 --camera realsense
+CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python3 train.py --log_dir logs/model/rep_log --batch_size 4 --learning_rate 0.001 --camera realsense
